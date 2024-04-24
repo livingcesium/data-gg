378b1447f83f45a299857ec2d5c0a359f6b58d4a
Dub3m <deneh@albany.edu>
2024-04-23 22:50:16 -0400

    Updated SignUp

--- app/src/components/SignUp.js
+++ app/src/components/SignUp.js
@@ -3,31 +3,31 @@ import { Link, useNavigate } from 'react-router-dom';
 import { handleSignUp } from '../handles'; // Ensure this path is correct based on your project structure
 import './App.css';
 
-
 function SignUp() {
   const navigate = useNavigate();
 
+  // Update the form when we submit
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    handleSignUp(event, () => navigate('/log-in')); 
+  };
+
   return (
-    <div>
-        <h1>Sign Up</h1>
-        <form onSubmit={() => handleSignUp(handleSignUp, () => navigate('/log-in'))}>
-
-            <div> 
-            <input className = "Text1" type="text" id="name-first" name="firstname" placeholder = "User ID" /><br/>
-            
-            <input className = "Text1" type="text" id="name-last" name="lastname" placeholder = "First Name" /><br/>
-          
-            <input className = "Text1" type="text" id="userid" name="username" placeholder = "Last Name" /><br/>
-
-            <input className = "Text1" type="password" id="password" name="password" placeholder = "Password" /><br/>
-           
-            <input className = "SubmitButton" type="submit" value="Submit"/>
+    <div className="AppContainer">
+        <h1 className="Text">Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input className="Text1" type="text" name="firstname" placeholder="First Name" /><br/>
+                <input className="Text1" type="text" name="lastname" placeholder="Last Name" /><br/>
+                <input className="Text1" type="text" name="username" placeholder="Username" /><br/>
+                <input className="Text1" type="password" name="password" placeholder="Password" /><br/>
+                <button type="submit" className="SubmitButton">Sign Up</button>
             </div>
-            <Link to="/log-in">Already have an account?</Link><br/>
+            <Link to="/log-in">Already have an account? Sign In</Link><br/>
             <Link to='/home'>Go Home</Link>
         </form>
     </div>
-    );
+  );
 }
 
 export default SignUp;
