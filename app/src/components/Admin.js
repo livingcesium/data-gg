93b8c0ce7fcb1f2a3c9347e2d153e9e17bd44737
Dub3m <deneh@albany.edu>
2024-04-25 14:21:09 -0400

    Updated Admin.js

--- app/src/components/Admin.js
+++ app/src/components/Admin.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 function Admin() {
     const [transactions, setTransactions] = useState([]);
-
+  
     useEffect(() => {
-        axios.get('http://localhost:9000/getTransactions')
-            .then(response => {
-                setTransactions(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error fetching transactions!', error);
-            });
+      axios.get('http://localhost:9000/getTransactions')
+          .then(response => {
+              setTransactions(response.data);
+          })
+          .catch(error => {
+              console.error('There was an error getting the transactions!', error);
+          });
     }, []);
-
+  
     return (
-        <div className="AppContainer">
-            <h1 className='Text'>Transactions</h1>
-            {transactions.map((transaction, index) => (
+      <div className="AppContainer">
+        <h1 className='Text'>Transactions</h1>
+        <div><Link to='/home'>Go Home</Link></div>
+        {transactions.map((transaction, index) => (
+            
+          <div className="List" key={index}>
+            <div><strong>Transaction {index + 1}</strong></div>
+            <div> ___________________________________</div>  
+            <div><strong>User ID:</strong> {transaction.user_id ? transaction.user_id._id : 'N/A'}</div>
+            <div><strong>User Name:</strong> {transaction.user_id ? `${transaction.user_id.firstname} ${transaction.user_id.lastname}` : 'N/A'}</div>
+            <div><strong>File ID:</strong> {transaction.file_id}</div>
+            <div><strong>Timestamp:</strong> {new Date(transaction.timeStamp).toLocaleString()}</div>
+            <div><strong>Uploaded:</strong> {transaction.up ? "Yes" : "No"}</div>
+          </div>
+        ))}
+        
+      </div>
 
-                <div className="List" key={index}>
-
-                    <div><strong>Transaction {index + 1}</strong></div> {/* Numbers each transaction in bold */}
-                    <div> ___________________________________</div>  
-                    <div><strong>User ID:</strong> {transaction.user_id._id}</div>
-                    <div><strong>User Name:</strong> {transaction.user_id.firstname} {transaction.user_id.lastname}</div>
-                    <div><strong>File ID:</strong> {transaction.file_id}</div>
-                    <div><strong>Timestamp:</strong> {new Date(transaction.timeStamp).toLocaleString()}</div>
-                    <div><strong>Uploaded:</strong> {transaction.up ? "Yes" : "No"}</div>
-
-                </div>
-
-            ))}
-        </div>
+      
     );
-}
+  }
 
-export default Admin;
+  export default Admin;
